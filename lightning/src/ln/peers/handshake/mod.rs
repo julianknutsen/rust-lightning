julianknutsen/rust-lightning

commit eb6a37144a2bf79d0db3793a428fc04454d4b75b

build scaffold for handshake module substitution support

lightning/src/ln/peers/handshake/mod.rs
@@ -1,0 +1,398 @@
+use bitcoin_hashes::{Hash, HashEngine};
+use bitcoin_hashes::sha256::Hash as Sha256;
+use rand::{Rng, thread_rng};
+use secp256k1::{PublicKey, SecretKey};
+
+use ln::peers::{chacha, hkdf};
+use ln::peers::conduit::Conduit;
+use ln::peers::handshake::acts::{ActOne, ActThree, ActTwo};
+use ln::peers::handshake::hash::HandshakeHash;
+use ln::peers::handshake::states::{ActOneExpectation, ActThreeExpectation, ActTwoExpectation, HandshakeState};
+
+mod acts;
+mod hash;
+mod states;
+mod tests;
+
+pub struct PeerHandshake {
+	state: Option<HandshakeState>,
+	private_key: SecretKey,
+
+	preset_ephemeral_private_key: Option<SecretKey>,
+	read_buffer: Vec<u8>,
+}
+
+impl PeerHandshake {
+	pub fn new(private_key: &SecretKey, ephemeral_private_key: Option<&SecretKey>) -> Self {
+		let preset_ephemeral_private_key = if let Some(key) = ephemeral_private_key {
+			// deref and clone
+			Some((*key).clone())
+		} else {
+			None
+		};
+
+		let handshake = PeerHandshake {
+			state: Some(HandshakeState::Blank),
+			private_key: (*private_key).clone(),
+			preset_ephemeral_private_key,
+			read_buffer: Vec::new(),
+		};
+		handshake
+	}
+
+	pub fn make_inbound(&mut self) {
+		let public_key = Self::private_key_to_public_key(&self.private_key);
+		let (hash, chaining_key) = Self::initialize_state(&public_key);
+		self.state = Some(HandshakeState::AwaitingActOne(ActOneExpectation {
+			hash,
+			chaining_key,
+		}))
+	}
+
+	fn initialize_state(public_key: &PublicKey) -> (HandshakeHash, [u8; 32]) {
+		// do the proper initialization
+		let protocol_name = b"Noise_XK_secp256k1_ChaChaPoly_SHA256";
+		let prologue = b"lightning";
+
+		let mut sha = Sha256::engine();
+		sha.input(protocol_name);
+		let chaining_key = Sha256::from_engine(sha).into_inner();
+
+		let mut initial_hash_preimage = chaining_key.to_vec();
+		initial_hash_preimage.extend_from_slice(prologue.as_ref());
+
+		let mut hash = HandshakeHash::new(initial_hash_preimage.as_slice());
+		hash.update(&public_key.serialize());
+
+		(hash, chaining_key) // hash, chaining_key
+	}
+
+	/// Process act dynamically
+	/// The role must be set before this method can be called
+	pub fn process_act(&mut self, input: &[u8], remote_public_key: Option<&PublicKey>) -> Result<(Vec<u8>, Option<Conduit>, Option<PublicKey>), String> {
+		let mut response: Vec<u8> = Vec::new();
+		let mut connected_peer = None;
+		let mut remote_pubkey = None;
+
+		self.read_buffer.extend_from_slice(input);
+		let read_buffer_length = self.read_buffer.len();
+
+		match &self.state {
+			Some(HandshakeState::Blank) => {
+				let remote_public_key = remote_public_key.ok_or("Call make_initiator() first")?;
+				let ephemeral_private_key = self.obtain_ephemeral_private_key();
+
+				let act_one = self.initiate(&ephemeral_private_key, &remote_public_key)?;
+				response = act_one.0.to_vec();
+			}
+			Some(HandshakeState::AwaitingActOne(_)) => {
+				let act_length = 50;
+				if read_buffer_length < act_length {
+					return Err("need at least 50 bytes".to_string());
+				}
+
+				let mut act_one_buffer = [0u8; 50];
+				act_one_buffer.copy_from_slice(&self.read_buffer[..act_length]);
+				self.read_buffer.drain(..act_length);
+
+				let ephemeral_private_key = self.obtain_ephemeral_private_key();
+
+				let act_two = self.process_act_one(ActOne(act_one_buffer), &ephemeral_private_key)?;
+				response = act_two.0.to_vec();
+			}
+			Some(HandshakeState::AwaitingActTwo(_)) => {
+				let act_length = 50;
+				if read_buffer_length < act_length {
+					return Err("need at least 50 bytes".to_string());
+				}
+
+				let mut act_two_buffer = [0u8; 50];
+				act_two_buffer.copy_from_slice(&self.read_buffer[..act_length]);
+				self.read_buffer.drain(..act_length);
+
+				let (act_three, mut conduit) = self.process_act_two(ActTwo(act_two_buffer))?;
+
+				if self.read_buffer.len() > 0 { // have we received more data still?
+					conduit.read(&self.read_buffer[..]);
+					self.read_buffer.drain(..);
+				}
+
+				response = act_three.0.to_vec();
+				connected_peer = Some(conduit);
+			}
+			Some(HandshakeState::AwaitingActThree(_)) => {
+				let act_length = 66;
+				if read_buffer_length < act_length {
+					return Err("need at least 50 bytes".to_string());
+				}
+
+				let mut act_three_buffer = [0u8; 66];
+				act_three_buffer.copy_from_slice(&self.read_buffer[..act_length]);
+				self.read_buffer.drain(..act_length);
+
+				let (public_key, mut conduit) = self.process_act_three(ActThree(act_three_buffer))?;
+
+				if self.read_buffer.len() > 0 { // have we received more data still?
+					conduit.read(&self.read_buffer[..]);
+					self.read_buffer.drain(..);
+				}
+
+				connected_peer = Some(conduit);
+				remote_pubkey = Some(public_key);
+			}
+			_ => {
+				return Err("no acts left to process".to_string());
+			}
+		};
+		Ok((response, connected_peer, remote_pubkey))
+	}
+
+	pub fn initiate(&mut self, ephemeral_private_key: &SecretKey, remote_public_key: &PublicKey) -> Result<ActOne, String> {
+		if let Some(HandshakeState::Blank) = &self.state {} else {
+			return Err("incorrect state".to_string());
+		}
+
+		let (mut hash, chaining_key) = Self::initialize_state(&remote_public_key);
+
+		// serialize act one
+		let (act_one, chaining_key, temporary_key) = self.calculate_act_message(
+			ephemeral_private_key,
+			remote_public_key,
+			chaining_key,
+			&mut hash,
+		);
+
+		self.state = Some(HandshakeState::AwaitingActTwo(ActTwoExpectation {
+			hash,
+			chaining_key,
+			temporary_key,
+			ephemeral_private_key: (*ephemeral_private_key).clone(),
+		}));
+
+		Ok(ActOne(act_one))
+	}
+
+	pub(crate) fn process_act_one(&mut self, act: ActOne, ephemeral_private_key: &SecretKey) -> Result<ActTwo, String> {
+		let state = self.state.take();
+		let act_one_expectation = match state {
+			Some(HandshakeState::AwaitingActOne(act_state)) => act_state,
+			Some(HandshakeState::Blank) => {
+				// this can also be initiated from a blank state
+				// public key
+				let public_key = Self::private_key_to_public_key(&self.private_key);
+				let (hash, chaining_key) = Self::initialize_state(&public_key);
+				ActOneExpectation {
+					hash,
+					chaining_key,
+				}
+			}
+			_ => {
+				self.state = state;
+				return Err("unexpected state".to_string());
+			}
+		};
+
+		let mut hash = act_one_expectation.hash;
+		let (remote_ephemeral_public_key, chaining_key, _) = self.process_act_message(
+			act.0,
+			&self.private_key,
+			act_one_expectation.chaining_key,
+			&mut hash,
+		)?;
+
+		let (act_two, chaining_key, temporary_key) = self.calculate_act_message(
+			ephemeral_private_key,
+			&remote_ephemeral_public_key,
+			chaining_key,
+			&mut hash,
+		);
+
+		self.state = Some(HandshakeState::AwaitingActThree(ActThreeExpectation {
+			hash,
+			chaining_key,
+			temporary_key,
+			ephemeral_private_key: (*ephemeral_private_key).clone(),
+			remote_ephemeral_public_key,
+		}));
+
+		Ok(ActTwo(act_two))
+	}
+
+	pub(crate) fn process_act_two(&mut self, act: ActTwo) -> Result<(ActThree, Conduit), String> {
+		let state = self.state.take();
+		let act_two_expectation = match state {
+			Some(HandshakeState::AwaitingActTwo(act_state)) => act_state,
+			_ => {
+				self.state = state;
+				return Err("unexpected state".to_string());
+			}
+		};
+
+		let mut hash = act_two_expectation.hash;
+		let (remote_ephemeral_public_key, chaining_key, temporary_key) = self.process_act_message(
+			act.0,
+			&act_two_expectation.ephemeral_private_key,
+			act_two_expectation.chaining_key,
+			&mut hash,
+		)?;
+
+		self.state = Some(HandshakeState::Complete);
+
+		// start serializing act three
+
+		let static_public_key = Self::private_key_to_public_key(&self.private_key);
+		let tagged_encrypted_pubkey = chacha::encrypt(&temporary_key, 1, &hash.value, &static_public_key.serialize());
+		hash.update(&tagged_encrypted_pubkey);
+
+		let ecdh = Self::ecdh(&self.private_key, &remote_ephemeral_public_key);
+		let (chaining_key, temporary_key) = hkdf::derive(&chaining_key, &ecdh);
+		let authentication_tag = chacha::encrypt(&temporary_key, 0, &hash.value, &[0; 0]);
+		let (sending_key, receiving_key) = hkdf::derive(&chaining_key, &[0; 0]);
+
+		let mut act_three_vec = [0u8].to_vec();
+		act_three_vec.extend_from_slice(&tagged_encrypted_pubkey);
+		act_three_vec.extend_from_slice(authentication_tag.as_slice());
+		let mut act_three = [0u8; 66];
+		act_three.copy_from_slice(act_three_vec.as_slice());
+
+		let connected_peer = Conduit {
+			sending_key,
+			receiving_key,
+			sending_chaining_key: chaining_key,
+			receiving_chaining_key: chaining_key,
+			sending_nonce: 0,
+			receiving_nonce: 0,
+			read_buffer: None,
+		};
+		Ok((ActThree(act_three), connected_peer))
+	}
+
+	pub(crate) fn process_act_three(&mut self, act: ActThree) -> Result<(PublicKey, Conduit), String> {
+		let state = self.state.take();
+		let act_three_expectation = match state {
+			Some(HandshakeState::AwaitingActThree(act_state)) => act_state,
+			_ => {
+				self.state = state;
+				return Err("unexpected state".to_string());
+			}
+		};
+
+		let version = act.0[0];
+		if version != 0 {
+			return Err("unexpected version".to_string());
+		}
+
+		let mut tagged_encrypted_pubkey = [0u8; 49];
+		tagged_encrypted_pubkey.copy_from_slice(&act.0[1..50]);
+
+		let mut chacha_tag = [0u8; 16];
+		chacha_tag.copy_from_slice(&act.0[50..66]);
+
+		let mut hash = act_three_expectation.hash;
+
+		let remote_pubkey_vec = chacha::decrypt(&act_three_expectation.temporary_key, 1, &hash.value, &tagged_encrypted_pubkey)?;
+		let mut remote_pubkey_bytes = [0u8; 33];
+		remote_pubkey_bytes.copy_from_slice(remote_pubkey_vec.as_slice());
+		// todo: replace unwrap with handleable error type
+		let remote_pubkey = PublicKey::from_slice(&remote_pubkey_bytes).unwrap();
+
+		hash.update(&tagged_encrypted_pubkey);
+
+		let ecdh = Self::ecdh(&act_three_expectation.ephemeral_private_key, &remote_pubkey);
+		let (chaining_key, temporary_key) = hkdf::derive(&act_three_expectation.chaining_key, &ecdh);
+		let _tag_check = chacha::decrypt(&temporary_key, 0, &hash.value, &chacha_tag)?;
+		let (receiving_key, sending_key) = hkdf::derive(&chaining_key, &[0; 0]);
+
+		let connected_peer = Conduit {
+			sending_key,
+			receiving_key,
+			sending_chaining_key: chaining_key,
+			receiving_chaining_key: chaining_key,
+			sending_nonce: 0,
+			receiving_nonce: 0,
+			read_buffer: None,
+		};
+		Ok((remote_pubkey, connected_peer))
+	}
+
+	fn obtain_ephemeral_private_key(&mut self) -> SecretKey {
+		if let Some(key) = self.preset_ephemeral_private_key.take() {
+			key
+		} else {
+			// generate a random ephemeral private key right here
+			let mut rng = thread_rng();
+			let mut ephemeral_bytes = [0; 32];
+			rng.fill_bytes(&mut ephemeral_bytes);
+			SecretKey::from_slice(&ephemeral_bytes).expect("You broke elliptic curve cryptography")
+		}
+	}
+
+	fn calculate_act_message(&self, local_private_key: &SecretKey, remote_public_key: &PublicKey, chaining_key: [u8; 32], hash: &mut HandshakeHash) -> ([u8; 50], [u8; 32], [u8; 32]) {
+		let local_public_key = Self::private_key_to_public_key(local_private_key);
+
+		hash.update(&local_public_key.serialize());
+
+		let ecdh = Self::ecdh(local_private_key, &remote_public_key);
+		let (chaining_key, temporary_key) = hkdf::derive(&chaining_key, &ecdh);
+		let tagged_ciphertext = chacha::encrypt(&temporary_key, 0, &hash.value, &[0; 0]);
+
+		hash.update(&tagged_ciphertext);
+
+		let mut act_vec = [0u8].to_vec();
+		act_vec.extend_from_slice(&local_public_key.serialize());
+		act_vec.extend_from_slice(tagged_ciphertext.as_slice());
+		let mut act = [0u8; 50];
+		act.copy_from_slice(act_vec.as_slice());
+		(act, chaining_key, temporary_key)
+	}
+
+	fn process_act_message(&self, act_bytes: [u8; 50], local_private_key: &SecretKey, chaining_key: [u8; 32], hash: &mut HandshakeHash) -> Result<(PublicKey, [u8; 32], [u8; 32]), String> {
+		let version = act_bytes[0];
+		if version != 0 {
+			return Err("unexpected version".to_string());
+		}
+
+		let mut ephemeral_public_key_bytes = [0u8; 33];
+		ephemeral_public_key_bytes.copy_from_slice(&act_bytes[1..34]);
+		// todo: replace unwrap with handleable error type
+		let ephemeral_public_key = PublicKey::from_slice(&ephemeral_public_key_bytes).unwrap();
+
+		let mut chacha_tag = [0u8; 16];
+		chacha_tag.copy_from_slice(&act_bytes[34..50]);
+
+		// process the act message
+
+		// update hash with partner's pubkey
+		hash.update(&ephemeral_public_key.serialize());
+
+		// calculate ECDH with partner's pubkey and local privkey
+		let ecdh = Self::ecdh(local_private_key, &ephemeral_public_key);
+
+		// HKDF(chaining key, ECDH) -> chaining key' + next temporary key
+		let (chaining_key, temporary_key) = hkdf::derive(&chaining_key, &ecdh);
+
+		// Validate chacha tag (temporary key, 0, self.hash, chacha_tag)
+		let _tag_check = chacha::decrypt(&temporary_key, 0, &hash.value, &chacha_tag)?;
+
+		hash.update(&chacha_tag);
+
+		Ok((ephemeral_public_key, chaining_key, temporary_key))
+	}
+
+	fn private_key_to_public_key(private_key: &SecretKey) -> PublicKey {
+		let curve = secp256k1::Secp256k1::new();
+		let pk_object = PublicKey::from_secret_key(&curve, &private_key);
+		pk_object
+	}
+
+	fn ecdh(private_key: &SecretKey, public_key: &PublicKey) -> [u8; 32] {
+		let curve = secp256k1::Secp256k1::new();
+		let mut pk_object = public_key.clone();
+		pk_object.mul_assign(&curve, &private_key[..]).expect("invalid multiplication");
+
+		let preimage = pk_object.serialize();
+		let mut sha = Sha256::engine();
+		sha.input(preimage.as_ref());
+		Sha256::from_engine(sha).into_inner()
+	}
+}

lightning/src/ln/peers/handshake/states.rs
@@ -1,0 +1,30 @@
+use ln::peers::handshake::hash::HandshakeHash;
+use secp256k1::{SecretKey, PublicKey};
+
+pub enum HandshakeState {
+	Blank,
+	AwaitingActOne(ActOneExpectation),
+	AwaitingActTwo(ActTwoExpectation),
+	AwaitingActThree(ActThreeExpectation),
+	Complete,
+}
+
+pub struct ActOneExpectation {
+	pub(super) hash: HandshakeHash,
+	pub(super) chaining_key: [u8; 32],
+}
+
+pub struct ActTwoExpectation {
+	pub(super) hash: HandshakeHash,
+	pub(super) chaining_key: [u8; 32],
+	pub(super) temporary_key: [u8; 32],
+	pub(super) ephemeral_private_key: SecretKey,
+}
+
+pub struct ActThreeExpectation {
+	pub(super) hash: HandshakeHash,
+	pub(super) chaining_key: [u8; 32],
+	pub(super) temporary_key: [u8; 32],
+	pub(super) ephemeral_private_key: SecretKey,
+	pub(super) remote_ephemeral_public_key: PublicKey,
+}

lightning/src/ln/mod.rs
@@ -13,6 +13,7 @@ pub mod channelmanager;
 pub mod channelmonitor;
 pub mod msgs;
 pub mod router;
+pub mod peers;
 pub mod peer_handler;
 pub mod chan_utils;
 pub mod features;
@@ -27,7 +28,8 @@ mod onion_utils;
 mod wire;
 
 #[cfg(test)]
-#[macro_use] mod functional_test_utils;
+#[macro_use]
+mod functional_test_utils;
 #[cfg(test)]
 mod functional_tests;
 #[cfg(test)]

lightning/src/ln/peers/mod.rs
@@ -1,0 +1,4 @@
+mod chacha;
+pub mod conduit;
+pub mod handshake;
+mod hkdf;

lightning/src/ln/peers/chacha.rs
@@ -1,0 +1,34 @@
+use util::chacha20poly1305rfc::ChaCha20Poly1305RFC;
+
+pub fn encrypt(key: &[u8], nonce: u64, associated_data: &[u8], plaintext: &[u8]) -> Vec<u8> {
+	let mut nonce_bytes = [0; 12];
+	nonce_bytes[4..].copy_from_slice(&nonce.to_le_bytes());
+
+	let mut chacha = ChaCha20Poly1305RFC::new(key, &nonce_bytes, associated_data);
+	let mut ciphertext = vec![0u8; plaintext.len()];
+	let mut authentication_tag = [0u8; 16];
+	chacha.encrypt(plaintext, &mut ciphertext, &mut authentication_tag);
+
+	let mut tagged_ciphertext = ciphertext;
+	tagged_ciphertext.extend_from_slice(&authentication_tag);
+	tagged_ciphertext
+}
+
+
+pub fn decrypt(key: &[u8], nonce: u64, associated_data: &[u8], tagged_ciphertext: &[u8]) -> Result<Vec<u8>, String> {
+	let mut nonce_bytes = [0; 12];
+	nonce_bytes[4..].copy_from_slice(&nonce.to_le_bytes());
+
+	let length = tagged_ciphertext.len();
+	let ciphertext = &tagged_ciphertext[0..length - 16];
+	let authentication_tag = &tagged_ciphertext[length - 16..length];
+
+	let mut chacha = ChaCha20Poly1305RFC::new(key, &nonce_bytes, associated_data);
+	let mut plaintext = vec![0u8; length - 16];
+	let success = chacha.decrypt(ciphertext, &mut plaintext, authentication_tag);
+	if success {
+		Ok(plaintext.to_vec())
+	} else {
+		Err("invalid hmac".to_string())
+	}
+}

commit b7bcbd0d218890a5378568b5c3540847123500af

fix: Allow partial act messages to be received
Fix regression introduced in eb6a371 that would error and subsequently
cause a disconnect if a partial act was sent to the state machine, even
if future calls could be added to the read buffer to create a
valid act.

Add appropriate unit tests and update fuzz testing to send partial acts
some of the time.

lightning/src/ln/peers/handshake/mod.rs
@@ -173,7 +173,8 @@ mod test {
 	#[test]
 	fn errors_properly_returned() {
 		let mut test_ctx = TestCtx::new();
-		assert_matches!(test_ctx.inbound_handshake.process_act(&[]).err(), Some(_));
+		let invalid_act1 = [0; 50];
+		assert_matches!(test_ctx.inbound_handshake.process_act(&invalid_act1).err(), Some(_));
 	}
 
 	// Test that any use of the PeerHandshake after returning an error panics
@@ -181,7 +182,8 @@ mod test {
 	#[should_panic(expected = "called `Option::unwrap()` on a `None` value")]
 	fn use_after_error_panics() {
 		let mut test_ctx = TestCtx::new();
-		assert_matches!(test_ctx.inbound_handshake.process_act(&[]).err(), Some(_));
+		let invalid_act1 = [0; 50];
+		assert_matches!(test_ctx.inbound_handshake.process_act(&invalid_act1).err(), Some(_));
 		test_ctx.inbound_handshake.process_act(&[]).unwrap();
 	}
 }

lightning/src/ln/peers/handshake/states.rs
@@ -181,6 +181,22 @@ impl IHandshakeState for ResponderAwaitingActOneState {
 		let mut read_buffer = self.read_buffer;
 		read_buffer.extend_from_slice(input);
 
+		// In the event of a partial fill, stay in the same state and wait for more data
+		if read_buffer.len() < ACT_ONE_TWO_LENGTH {
+			return Ok((
+				None,
+				HandshakeState::ResponderAwaitingActOne(Self {
+					responder_static_private_key: self.responder_static_private_key,
+					responder_ephemeral_private_key: self.responder_ephemeral_private_key,
+					responder_ephemeral_public_key: self.responder_ephemeral_public_key,
+					chaining_key: self.chaining_key,
+					hash: self.hash,
+					read_buffer
+				})
+			));
+		}
+
+
 		let hash = self.hash;
 		let responder_static_private_key = self.responder_static_private_key;
 		let chaining_key = self.chaining_key;
@@ -221,6 +237,22 @@ impl IHandshakeState for InitiatorAwaitingActTwoState {
 	fn next(self, input: &[u8]) -> Result<(Option<Vec<u8>>, HandshakeState), String> {
 		let mut read_buffer = self.read_buffer;
 		read_buffer.extend_from_slice(input);
+
+		// In the event of a partial fill, stay in the same state and wait for more data
+		if read_buffer.len() < ACT_ONE_TWO_LENGTH {
+			return Ok((
+				None,
+				HandshakeState::InitiatorAwaitingActTwo(Self {
+					initiator_static_private_key: self.initiator_static_private_key,
+					initiator_static_public_key: self.initiator_static_public_key,
+					initiator_ephemeral_private_key: self.initiator_ephemeral_private_key,
+					responder_static_public_key: self.responder_static_public_key,
+					chaining_key: self.chaining_key,
+					hash: self.hash,
+					read_buffer
+				})
+			));
+		}
 
 		let initiator_static_private_key = self.initiator_static_private_key;
 		let initiator_static_public_key = self.initiator_static_public_key;
@@ -287,8 +319,18 @@ impl IHandshakeState for ResponderAwaitingActThreeState {
 		let mut read_buffer = self.read_buffer;
 		read_buffer.extend_from_slice(input);
 
+		// In the event of a partial fill, stay in the same state and wait for more data
 		if read_buffer.len() < ACT_THREE_LENGTH {
-			return Err("need at least 66 bytes".to_string());
+			return Ok((
+				None,
+				HandshakeState::ResponderAwaitingActThree(Self {
+					hash: self.hash,
+					responder_ephemeral_private_key: self.responder_ephemeral_private_key,
+					chaining_key: self.chaining_key,
+					temporary_key: self.temporary_key,
+					read_buffer
+				})
+			));
 		}
 
 		let hash = self.hash;
@@ -407,10 +449,6 @@ fn calculate_act_message(local_private_ephemeral_key: &SecretKey, local_public_e
 // https://github.com/lightningnetwork/lightning-rfc/blob/master/08-transport.md#act-two (receiver)
 fn process_act_message(read_buffer: &mut Vec<u8>, local_private_key: &SecretKey, chaining_key: ChainingKey, hash: Sha256) -> Result<(PublicKey, Sha256, SymmetricKey, SymmetricKey), String> {
 	// 1. Read exactly 50 bytes from the network buffer
-	if read_buffer.len() < ACT_ONE_TWO_LENGTH {
-		return Err("need at least 50 bytes".to_string());
-	}
-
 	let act_bytes: Vec<u8> = read_buffer.drain(..ACT_ONE_TWO_LENGTH).collect();
 
 	// 2.Parse the read message (m) into v, re, and c
@@ -503,9 +541,9 @@ mod test {
 			let responder = ResponderAwaitingActOneState::new(responder_static_private_key, responder_ephemeral_private_key);
 
 			TestCtx {
-				initiator: HandshakeState::InitiatorStarting(initiator),
+				initiator: InitiatorStarting(initiator),
 				initiator_public_key,
-				responder: HandshakeState::ResponderAwaitingActOne(responder),
+				responder: ResponderAwaitingActOne(responder),
 				responder_static_public_key,
 				valid_act1: hex::decode("00036360e856310ce5d294e8be33fc807077dc56ac80d95d9cd4ddbd21325eff73f70df6086551151f58b8afe6c195782c6a").unwrap(),
 				valid_act2: hex::decode("0002466d7fcae563e5cb09a0d1870bb580344804617879a14949cf22285f1bae3f276e2470b93aac583c9ef6eafca3f730ae").unwrap(),
@@ -565,8 +603,6 @@ mod test {
 	// Responder::AwaitingActOne -> AwaitingActThree
 	// TODO: Should this fail since we don't expect data > ACT_ONE_TWO_LENGTH and likely indicates
 	// a bad peer?
-	// TODO: Should the behavior be changed to handle act1 data that is striped across multiple
-	// next() calls?
 	#[test]
 	fn awaiting_act_one_to_awaiting_act_three_input_extra_bytes() {
 		let test_ctx = TestCtx::new();
@@ -578,13 +614,18 @@ mod test {
 		assert_matches!(awaiting_act_three_state, ResponderAwaitingActThree(_));
 	}
 
-	// Responder::AwaitingActOne -> Error (input too small)
+	// Responder::AwaitingActOne -> AwaitingActThree (segmented calls)
 	// RFC test vector: transport-responder act1 short read test
+	// Divergence from RFC tests due to not reading directly from the socket (partial message OK)
 	#[test]
-	fn awaiting_act_one_to_awaiting_act_three_input_too_small() {
+	fn awaiting_act_one_to_awaiting_act_three_segmented() {
 		let test_ctx = TestCtx::new();
-		let act1 = hex::decode("00036360e856310ce5d294e8be33fc807077dc56ac80d95d9cd4ddbd21325eff73f70df6086551151f58b8afe6c195782c").unwrap();
-		assert_eq!(test_ctx.responder.next(&act1).err(), Some(String::from("need at least 50 bytes")))
+		let act1_partial1 = &test_ctx.valid_act1[..25];
+		let act1_partial2 = &test_ctx.valid_act1[25..];
+
+		let next_state = test_ctx.responder.next(&act1_partial1).unwrap();
+		assert_matches!(next_state, (None, ResponderAwaitingActOne(_)));
+		assert_matches!(next_state.1.next(&act1_partial2).unwrap(), (Some(_), ResponderAwaitingActThree(_)));
 	}
 
 	// Responder::AwaitingActOne -> Error (bad version byte)
@@ -659,15 +700,20 @@ mod test {
 		assert_eq!(100, conduit.decryptor.read_buffer_length());
 	}
 
-	// Initiator::AwaitingActTwo -> Error (input too small)
+	// Initiator::AwaitingActTwo -> Complete (segmented calls)
 	// RFC test vector: transport-initiator act2 short read test
+	// Divergence from RFC tests due to not reading directly from the socket (partial message OK)
 	#[test]
-	fn awaiting_act_two_input_too_small() {
+	fn awaiting_act_two_to_complete_segmented() {
 		let test_ctx = TestCtx::new();
 		let (_act1, awaiting_act_two_state) = do_next_or_panic!(test_ctx.initiator, &[]);
-		let act2 = hex::decode("0002466d7fcae563e5cb09a0d1870bb580344804617879a14949cf22285f1bae3f276e2470b93aac583c9ef6eafca3f730").unwrap();
 
-		assert_eq!(awaiting_act_two_state.next(&act2).err(), Some(String::from("need at least 50 bytes")));
+		let act2_partial1 = &test_ctx.valid_act2[..25];
+		let act2_partial2 = &test_ctx.valid_act2[25..];
+
+		let next_state = awaiting_act_two_state.next(&act2_partial1).unwrap();
+		assert_matches!(next_state, (None, InitiatorAwaitingActTwo(_)));
+		assert_matches!(next_state.1.next(&act2_partial2).unwrap(), (Some(_), Complete(_)));
 	}
 
 	// Initiator::AwaitingActTwo -> Error (bad version byte)
@@ -751,15 +797,20 @@ mod test {
 		assert_eq!(awaiting_act_three_state.next(&act3).err(), Some(String::from("unexpected version")));
 	}
 
-	// Responder::AwaitingActThree -> Error (input too small)
+	// Responder::AwaitingActThree -> Complete (segmented calls)
 	// RFC test vector: transport-responder act3 short read test
+	// Divergence from RFC tests due to not reading directly from the socket (partial message OK)
 	#[test]
-	fn awaiting_act_three_input_too_small() {
+	fn awaiting_act_three_to_complete_segmented() {
 		let test_ctx = TestCtx::new();
 		let (_act2, awaiting_act_three_state) = do_next_or_panic!(test_ctx.responder, &test_ctx.valid_act1);
-		let act3 = hex::decode("00b9e3a702e93e3a9948c2ed6e5fd7590a6e1c3a0344cfc9d5b57357049aa22355361aa02e55a8fc28fef5bd6d71ad0c38228dc68b1c466263b47fdf31e560e139").unwrap();
 
-		assert_eq!(awaiting_act_three_state.next(&act3).err(), Some(String::from("need at least 66 bytes")));
+		let act3_partial1 = &test_ctx.valid_act3[..35];
+		let act3_partial2 = &test_ctx.valid_act3[35..];
+
+		let next_state = awaiting_act_three_state.next(&act3_partial1).unwrap();
+		assert_matches!(next_state, (None, ResponderAwaitingActThree(_)));
+		assert_matches!(next_state.1.next(&act3_partial2), Ok((None, Complete(_))));
 	}
 
 	// Responder::AwaitingActThree -> Error (invalid hmac)