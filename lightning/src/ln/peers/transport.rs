c19e053e927792fbf7b79dadd74bcf019259a3bf
Julian Knutsen <julianknutsen@users.noreply.github.com>
2020-09-01 19:53:33 -0700

    refactor: Make Transport the source of their_node_id
    This patch ontinues to separate state the exists before NOISE is
    complete and after it is complete to unlock future refactoring.
    
    Most callers immediately unwrapped the value from Peer and
    can just call Transport::get_their_node_id().
    
    The duplicate connection disconnect path has been rewritten
    to determine whether or not to remove & send a disconnect event without
    needing to use a None value for Option<PublicKey>
    
    All other users are in contexts where they either exit early or continue
    if !transport.is_connected() so it is also safe to call
    Transport::get_their_node_id()

--- lightning/src/ln/peers/transport.rs
+++ lightning/src/ln/peers/transport.rs
@@ -31,7 +31,7 @@ pub(super) trait IPeerHandshake {
 pub(super) struct Transport<PeerHandshakeImpl: IPeerHandshake=PeerHandshake> {
 	pub(super) conduit: Option<Conduit>,
 	handshake: PeerHandshakeImpl,
-	pub(super) their_node_id: Option<PublicKey>,
+	their_node_id: Option<PublicKey>,
 }
 
 impl<PeerHandshakeImpl: IPeerHandshake> ITransport for Transport<PeerHandshakeImpl> {
@@ -168,6 +168,11 @@ impl<PeerHandshakeImpl: IPeerHandshake> ITransport for Transport<PeerHandshakeIm
 			self.enqueue_message(message, output_buffer, logger);
 		}
 	}
+
+	fn get_their_node_id(&self) -> PublicKey {
+		assert!(self.is_connected(), "Retrieving the remote node_id is only supported after transport is connected");
+		self.their_node_id.unwrap()
+	}
 }
 
 #[cfg(test)]
@@ -262,6 +267,42 @@ mod tests {
 		assert!(transport.is_connected());
 	}
 
+	// Test get_their_node_id() in unconnected and connected scenarios
+	#[test]
+	#[should_panic(expected = "Retrieving the remote node_id is only supported after transport is connected")]
+	fn inbound_unconnected_get_their_node_id_panics() {
+		let transport = create_inbound_for_test::<PeerHandshakeTestStubFail>();
+
+		let _should_panic = transport.get_their_node_id();
+	}
+
+	#[test]
+	#[should_panic(expected = "Retrieving the remote node_id is only supported after transport is connected")]
+	fn outbound_unconnected_get_their_node_id_panics() {
+		let mut transport = create_outbound_for_test::<PeerHandshakeTestStubFail>();
+		transport.set_up_outbound();
+
+		let _should_panic = transport.get_their_node_id();
+	}
+
+	#[test]
+	fn inbound_unconnected_get_their_node_id() {
+		let mut transport = create_inbound_for_test::<PeerHandshakeTestStubComplete>();
+		let mut spy = QueueableSpy::new();
+
+		transport.process_input(&[], &mut spy).unwrap();
+		let _no_panic = transport.get_their_node_id();
+	}
+
+	#[test]
+	fn outbound_unconnected_get_their_node_id() {
+		let mut transport = create_inbound_for_test::<PeerHandshakeTestStubComplete>();
+		let mut spy = QueueableSpy::new();
+
+		transport.process_input(&[], &mut spy).unwrap();
+		let _no_panic = transport.get_their_node_id();
+	}
+
 	// Test that when a handshake completes is_connected() is correct
 	#[test]
 	fn outbound_handshake_complete_ready_for_encryption() {
@@ -270,6 +311,7 @@ mod tests {
 
 		transport.process_input(&[], &mut spy).unwrap();
 		assert!(transport.is_connected());
+		let _no_panic = transport.get_their_node_id();
 	}
 
 	#[test]
@@ -391,7 +433,7 @@ mod tests {
 		let logger = TestLogger::new();
 		let mut transport = create_inbound_for_test::<PeerHandshakeTestStubComplete>();
 		let mut spy = QueueableSpy::new();
-		assert_eq!(Ok(true), transport.process_input(&[], &mut spy));
+		transport.process_input(&[], &mut spy).unwrap();
 
 		let ping = msgs::Ping {
 			ponglen: 0,
@@ -406,7 +448,7 @@ mod tests {
 		let logger = TestLogger::new();
 		let mut transport = create_outbound_for_test::<PeerHandshakeTestStubComplete>();
 		let mut spy = QueueableSpy::new();
-		assert_eq!(Ok(true), transport.process_input(&[], &mut spy));
+		transport.process_input(&[], &mut spy).unwrap();
 
 		let ping = msgs::Ping {
 			ponglen: 0,
